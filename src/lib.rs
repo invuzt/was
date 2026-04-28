e6ae69cc141535a91eb100fb2eda6208794b7e9f
invuzt <invuzt@gmail.com>
2026-04-28 09:28:25 +0000

    fix: full reset to index.html and modern deployment

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,5 @@
 #![no_std]
-
 #[no_mangle]
-pub extern "C" fn check_wasm(val: i32) -> i32 {
-    val + 1
-}
-
+pub extern "C" fn check_wasm(val: i32) -> i32 { val + 1 }
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
+fn panic(_info: &core::panic::PanicInfo) -> ! { loop {} }
