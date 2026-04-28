3983fc2b90a5d6505102d59a5561ce255717c82a
invuzt <invuzt@gmail.com>
2026-04-28 10:00:39 +0000

    feat: make 3D cube interactive with touch and mouse

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,25 @@
 #![no_std]
 
+static mut ANGLE_X: f32 = 0.0;
+static mut ANGLE_Y: f32 = 0.0;
+
 #[no_mangle]
-pub extern "C" fn get_face_z(z1: f32, z2: f32, z3: f32, z4: f32) -> f32 {
-    (z1 + z2 + z3 + z4) / 4.0
+pub extern "C" fn update_rotation(dx: f32, dy: f32) {
+    unsafe {
+        ANGLE_Y += dx * 0.01;
+        ANGLE_X += dy * 0.01;
+    }
 }
 
 #[no_mangle]
-pub extern "C" fn calculate_shading(z_depth: f32) -> i32 {
-    // Makin jauh (z besar), makin gelap (nilai 0-255)
-    let shade = 200.0 - (z_depth * 40.0);
-    if shade < 20.0 { 20 } else if shade > 255.0 { 255 } else { shade as i32 }
+pub extern "C" fn get_angle_x() -> f32 { unsafe { ANGLE_X } }
+
+#[no_mangle]
+pub extern "C" fn get_angle_y() -> f32 { unsafe { ANGLE_Y } }
+
+#[no_mangle]
+pub extern "C" fn get_face_z(z1: f32, z2: f32, z3: f32, z4: f32) -> f32 {
+    (z1 + z2 + z3 + z4) / 4.0
 }
 
 #[panic_handler]
