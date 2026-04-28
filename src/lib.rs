1df4c1efadf29a2569a2af2bb92419ef49242ec9
invuzt <invuzt@gmail.com>
2026-04-28 10:06:00 +0000

    feat: switch to GPU rendering using WebGL

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
 #![no_std]
-static mut ANGLE_X: f32 = 0.0;
-static mut ANGLE_Y: f32 = 0.0;
 
 #[no_mangle]
-pub extern "C" fn update_rotation(dx: f32, dy: f32) {
-    unsafe { ANGLE_Y += dx * 0.01; ANGLE_X += dy * 0.01; }
+pub extern "C" fn get_rotation_matrix(angle: f32, out_ptr: *mut f32) {
+    let s = libm::sinf(angle);
+    let c = libm::cosf(angle);
+    unsafe {
+        // Matrix 4x4 Rotasi Y sederhana
+        *out_ptr.add(0) = c;    *out_ptr.add(1) = 0.0;  *out_ptr.add(2) = s;   *out_ptr.add(3) = 0.0;
+        *out_ptr.add(4) = 0.0;  *out_ptr.add(5) = 1.0;  *out_ptr.add(6) = 0.0; *out_ptr.add(7) = 0.0;
+        *out_ptr.add(8) = -s;   *out_ptr.add(9) = 0.0;  *out_ptr.add(10) = c;  *out_ptr.add(11) = 0.0;
+        *out_ptr.add(12) = 0.0; *out_ptr.add(13) = 0.0; *out_ptr.add(14) = 0.0; *out_ptr.add(15) = 1.0;
+    }
 }
-#[no_mangle]
-pub extern "C" fn get_angle_x() -> f32 { unsafe { ANGLE_X } }
-#[no_mangle]
-pub extern "C" fn get_angle_y() -> f32 { unsafe { ANGLE_Y } }
-#[no_mangle]
-pub extern "C" fn get_z_avg(z1: f32, z2: f32, z3: f32) -> f32 { (z1 + z2 + z3) / 3.0 }
+
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! { loop {} }
