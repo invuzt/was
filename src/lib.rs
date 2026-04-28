737e3c5f64e50c8c8484bff9b23c2b3db8d856ee
invuzt <invuzt@gmail.com>
2026-04-28 09:55:35 +0000

    fix: add libm for no_std math and enable action caching

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 #![no_std]
 
 #[no_mangle]
-pub extern "C" fn rotate_3d(x: f32, y: f32, z: f32, angle: f32, axis: i32) -> f32 {
-    // Sederhana: Sin/Cos Approximation (biar tetap no_std)
-    let s = angle.sin();
-    let c = angle.cos();
-    
-    match axis {
-        0 => x, // Axis X
-        1 => y * c - z * s, // Axis Y
-        2 => y * s + z * c, // Axis Z
-        _ => 0.0
-    }
+pub extern "C" fn rotate_x(y: f32, z: f32, angle: f32) -> f32 {
+    let s = libm::sinf(angle);
+    let c = libm::cosf(angle);
+    y * c - z * s
+}
+
+#[no_mangle]
+pub extern "C" fn rotate_y(x: f32, z: f32, angle: f32) -> f32 {
+    let s = libm::sinf(angle);
+    let c = libm::cosf(angle);
+    x * c + z * s
 }
 
 #[panic_handler]
