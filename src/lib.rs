9d1df6b061af017c180cb145654d5be66f8ce698
invuzt <invuzt@gmail.com>
2026-04-28 09:58:07 +0000

    feat: solid 3D with z-sorting and shading

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,15 @@
 #![no_std]
 
 #[no_mangle]
-pub extern "C" fn rotate_x(y: f32, z: f32, angle: f32) -> f32 {
-    let s = libm::sinf(angle);
-    let c = libm::cosf(angle);
-    y * c - z * s
+pub extern "C" fn get_face_z(z1: f32, z2: f32, z3: f32, z4: f32) -> f32 {
+    (z1 + z2 + z3 + z4) / 4.0
 }
 
 #[no_mangle]
-pub extern "C" fn rotate_y(x: f32, z: f32, angle: f32) -> f32 {
-    let s = libm::sinf(angle);
-    let c = libm::cosf(angle);
-    x * c + z * s
+pub extern "C" fn calculate_shading(z_depth: f32) -> i32 {
+    // Makin jauh (z besar), makin gelap (nilai 0-255)
+    let shade = 200.0 - (z_depth * 40.0);
+    if shade < 20.0 { 20 } else if shade > 255.0 { 255 } else { shade as i32 }
 }
 
 #[panic_handler]
