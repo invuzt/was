cabd9323342fc3f0c2d6f570d92efdf07e4af29e
invuzt <invuzt@gmail.com>
2026-04-28 10:04:15 +0000

    feat: change model to interactive pyramid

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,16 @@
 #![no_std]
-
 static mut ANGLE_X: f32 = 0.0;
 static mut ANGLE_Y: f32 = 0.0;
 
 #[no_mangle]
 pub extern "C" fn update_rotation(dx: f32, dy: f32) {
-    unsafe {
-        ANGLE_Y += dx * 0.01;
-        ANGLE_X += dy * 0.01;
-    }
+    unsafe { ANGLE_Y += dx * 0.01; ANGLE_X += dy * 0.01; }
 }
-
 #[no_mangle]
 pub extern "C" fn get_angle_x() -> f32 { unsafe { ANGLE_X } }
-
 #[no_mangle]
 pub extern "C" fn get_angle_y() -> f32 { unsafe { ANGLE_Y } }
-
 #[no_mangle]
-pub extern "C" fn get_face_z(z1: f32, z2: f32, z3: f32, z4: f32) -> f32 {
-    (z1 + z2 + z3 + z4) / 4.0
-}
-
+pub extern "C" fn get_z_avg(z1: f32, z2: f32, z3: f32) -> f32 { (z1 + z2 + z3) / 3.0 }
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! { loop {} }
